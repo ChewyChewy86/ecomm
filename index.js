ae246d420800dea7ae3e22656a280243be270b64
unknown <trainguitar@gmail.com>
2020-05-04 21:18:56 +0100

    App complete

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
+// Dependencies/API's
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+
+// Routes
+const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
+const adminProductsRouter = require("./routes/admin/products");
 
 const app = express();
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["sd8129jdksjdlj2910dsjakd"],
+  })
+);
+app.use(authRouter);
+app.use(productsRouter);
+app.use(cartsRouter);
+app.use(adminProductsRouter);
 
-app.get("/", (req, res) => {
-  res.send(`
-    <div>
-      <form method="POST">
-        <input name="email" placeholder="email"/>
-        <input name="password" placeholder="password"/>
-        <input name="passwordConfirmation" placeholder="password confirmation"/>
-        <button>Sign Up</button>
-      </form>
-    </div>`);
-});
-
-app.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("Account created");
-});
 
 app.listen(3000, () => {
   console.log("Listening");
